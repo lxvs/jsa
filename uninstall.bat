d41ff17043cdb95520999cf88347e8f670e582df
lxvs <jn.apsd@gmail.com>
2022-08-06 14:53:27 +0800

    Fix that first installation messes up environment variables

--- uninstall.bat
+++ uninstall.bat
@@ -45,11 +45,11 @@ call:getreg "%regpath%" "path" installation
 setlocal EnableDelayedExpansion
 if defined UserPath (
     if not defined silent (
-        setx Path "!UserPath:%installation%;=!" 1>nul
+        if defined installation (setx Path "!UserPath:%installation%;=!" 1>nul)
         reg delete "%regpath%" /f 1>nul
         reg delete "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\%name%.exe" /f 1>nul
     ) else (
-        setx Path "!UserPath:%installation%;=!" 1>nul 2>&1
+        if defined installation (setx Path "!UserPath:%installation%;=!" 1>nul 2>&1)
         reg delete "%regpath%" /f 1>nul 2>&1
         reg delete "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\%name%.exe" /f 1>nul 2>&1
     )
