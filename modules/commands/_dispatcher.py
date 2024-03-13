8d737c0b076cbdd888115d4a12c1049abe5d88be
Liu, Zhao-hui <lzhh@duck.com>
2024-03-13 17:24:21 +0800

    Implement sleep command

--- modules/commands/_dispatcher.py
+++ modules/commands/_dispatcher.py
@@ -1,10 +1,13 @@
 from modules.commands._command import JsaCommand
 from modules.commands.autosol import Autosol
+from modules.commands.sleep import Sleep
 
 class JsaCommandDispatcher:
     @staticmethod
     def get_instance(name: str) -> JsaCommand:
         if name == 'autosol':
             return Autosol()
+        if name == 'sleep':
+            return Sleep()
         else:
             return None
